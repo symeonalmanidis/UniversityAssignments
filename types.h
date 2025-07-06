e6d437d2d8713185f7fd50fa4aec0a2304eb59b3
symeonalmanidis <ics23083@uom.edu.gr>
2025-07-06 16:27:37 +0300

    yo

---
+++ types.h
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#ifndef _Lang_types_h
+#define _Lang_types_h
+
+/* Definition of the supported types*/
+typedef enum {type_error, 
+              type_integer, type_bool,
+              type_int_set,
+              type_int_array, type_bool_array 
+              } ParType;
+
+#endif              
